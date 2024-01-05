cc644f1ce0d2c2e81bbd86328e93d236b96ff368
SSangRRae <pepe0003@kakao.com>
2024-01-06 00:23:15 +0900

    feat: CASE3

---
+++ TVC/Case3TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  Case3TableViewCell.swift
+//  TVC
+//
+//  Created by SangRae Kim on 1/5/24.
+//
+
+import UIKit
+
+class Case3TableViewCell: UITableViewCell {
+    @IBOutlet var checkImageView: UIImageView!
+    @IBOutlet var todoLabel: UILabel!
+    @IBOutlet var starImageView: UIImageView!
+}
