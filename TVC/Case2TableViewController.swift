4605b9e54f293eab54b38ce9d35817894461ac4e
SSangRRae <pepe0003@kakao.com>
2024-01-12 11:45:05 +0900

    refactor: "String Array" to "enum"

--- TVC/Case2TableViewController.swift
+++ TVC/Case2TableViewController.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
+enum Setting: Int, CaseIterable {
+    case total
+    case personal
+    case others
+    
+    var cellData: [String] {
+        switch self {
+        case .total:
+            return ["공지사항", "실험실", "버전 정보"]
+        case .personal:
+            return ["개인/보안", "알림", "채팅", "멀티프로필"]
+        case .others:
+            return ["전체 설정", "개인 설정", "기타"]
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .total:
+            return "전체 설정"
+        case .personal:
+            return "개인 설정"
+        case .others:
+            return "기타"
+        }
+    }
+}
+
 class Case2TableViewController: UITableViewController {
     
     @IBOutlet var titleLabel: UILabel!
-    let titles = [
-        ["공지사항", "실험실", "버전 정보"],
-        ["개인/보안", "알림", "채팅", "멀티프로필"],
-        ["고객센터/도움말"]]
-    let headers = ["전체 설정", "개인 설정", "기타"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,27 +47,27 @@ class Case2TableViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return titles.count
+        return Setting.allCases.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Setting.allCases[section].title
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titles[section].count
+        return Setting.allCases[section].cellData.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Case2TableViewCell")
         
-        cell?.textLabel?.text = titles[indexPath.section][indexPath.row]
         cell?.textLabel?.font = .systemFont(ofSize: 16)
+        cell?.textLabel?.text = Setting.allCases[indexPath.section].cellData[indexPath.row]
         
         return cell!
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 50
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return headers[section]
     }
 }
